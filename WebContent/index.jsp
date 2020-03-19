2e5e2fd3657382000c265224cf634c9f5761ae26
bigblackcat <blackcat>
2020-03-19 12:52:55 +0800

    first

---
+++ WebContent/index.jsp
@@ -0,0 +1,16 @@
+<%@ page language="java" contentType="text/html; charset=ISO-8859-1"
+         pageEncoding="ISO-8859-1" %>
+<!DOCTYPE html>
+<html>
+<head>
+    <meta charset="ISO-8859-1">
+    <title>Insert title here</title>
+</head>
+<body>
+<from action="./user/login" method="post">
+    <input name="un">
+    <input name="pw">
+    <input type="submit" value="登录">
+</from>
+</body>
+</html>
---
+++ src/demo/loginServlet.java
@@ -0,0 +1,30 @@
+package demo;
+
+import java.io.IOException;
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+/**
+ * Servlet implementation class loginServlet
+ */
+@WebServlet("/loginServlet")
+public class loginServlet extends HttpServlet {
+
+
+    protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+        String name = request.getParameter("un");
+        String pass = request.getParameter("pw");
+        System.out.println(name);
+        System.out.println(pass);
+    }
+
+
+    protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+        // TODO Auto-generated method stub
+        doGet(request, response);
+    }
+
+}
